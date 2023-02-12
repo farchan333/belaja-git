7fdaf2fc08cf5f2d30f6248a61aee2683070ca5d
Farchan <mfarchan32@gmail.com>
2023-02-12 12:14:04 +0800

    commit ke 3

--- MantepEuy.java
+++ MantepEuy.java
@@ -1,5 +1,8 @@
 public class MantepEuy {
     public static void main(String[] args) {
         System.out.println("Nama saya farchan");
+        System.out.println("Nama : Farchan");
+        System.out.println("Nim : 20650121");
+        System.out.println("Kelas : D");
     }
 }
