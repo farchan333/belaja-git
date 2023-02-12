802ff401e91407e8476d49aead59457b8b4bc12e
Farchan <mfarchan32@gmail.com>
2023-02-12 12:30:02 +0800

    add ignore

--- MantepEuy.java
+++ MantepEuy.java
@@ -4,5 +4,6 @@ public class MantepEuy {
         System.out.println("Nama : Farchan");
         System.out.println("Nim : 20650121");
         System.out.println("Kelas : D");
+        System.out.println("waoww kerenn kaleeeq");
     }
 }
--- tes.java
+++ tes.java
@@ -1,5 +1,9 @@
 public class tes {
     public static void main(String[] args) {
         System.out.println("Hallo");
+        System.out.println("hehehe");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Farchan");
+        }
     }
 }
