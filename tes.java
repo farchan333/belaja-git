03366a59a379901b1969f64daebe9e1ee966c730
Farchan <mfarchan32@gmail.com>
2023-02-12 14:18:53 +0800

    commit

--- tes.java
+++ tes.java
@@ -2,8 +2,6 @@ public class tes {
     public static void main(String[] args) {
         System.out.println("Hallo");
         System.out.println("hehehe");
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Farchan");
-        }
+        System.out.print("fix buxed");
     }
 }
